b56a7178d24645a07ee4f711dd122f0861b5ddc9
NRothera <31472463+NRothera@users.noreply.github.com>
2017-10-31 15:07:35 +0000

    finished code

---
+++ main.js
@@ -0,0 +1,113 @@
+//
+// var outsideVariable =5;
+//
+// function doSomething(){
+//   var outsideVariable=10;
+//   console.log(outsideVariable);
+// }
+//
+// doSomething();
+//
+//
+
+// function firstFunction(){
+//   console.log("I am the first function")
+// }
+//
+// function secondFunction(){
+//   firstFunction();
+//   console.log("I am the second function");
+//   function thirdFunction(){
+//     console.log("I am a function within a function")
+//   }
+//   thirdFunction()
+// }
+//
+//
+// secondFunction()
+
+// function joinStringAndPrint(string1,string2){
+//   var newString = string1+string2
+//
+//   function printString(string){
+//     console.log(string);
+//   }
+//   printString( newString)
+//   return newString
+// }
+//
+// var hello = "hello, ";
+// var world = "world!";
+//
+// console.log(joinStringAndPrint(hello,world))
+//
+
+// function joinStringsAndPrint( string1 , string2 ) {
+//
+// 	var newString =  string1 + string2;
+//
+// 	function printString( string ) {
+//
+// 		// console.log(string);
+//
+// 		// Q3
+// 		// console.log(newString);
+//     //yes
+// 		// Q4
+// 		// console.log(string1 + string2);
+//     //yes
+// 	}
+//
+// 	printString( newString );
+//
+// 	return newString;
+//
+//
+// }
+// var string1 = "hello, ";
+// var string2 = "world";
+// joinStringsAndPrint( string1 , string2 );
+
+// Q1
+// printString( "run from outside joinStrings" );
+//no
+
+
+// Q2
+// console.log(newString)
+//no
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
